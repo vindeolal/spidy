35df1efe55a9d9eb3c626a6151b2ad8299b0a9e4
Vinod Deolal <vinoddeolal@gmail.com>
2020-01-26 19:19:29 +0530

    ability to play any video by name from youtube command <play song song-name>

--- playSong.py
+++ playSong.py
@@ -1,18 +1,44 @@
+import re
+import sys
+
 import pafy
 import vlc
 
 from textToSpeech import text_to_speech
 
 # make sure vlc player is installed in the system use sudo apt-get install vlc for ubuntu
-# TODO : make url dynamic based on song name/video name in the command
+import urllib.request
+from bs4 import BeautifulSoup
 
-url = "https://www.youtube.com/watch?v=JGwWNGJdvx8"
-video_url = pafy.new(url)
-text_to_speech('playing song')
-best = video_url.getbest()
-vlc_instance = vlc.Instance()
-vlc_player = vlc_instance.media_player_new()
-video = vlc_instance.media_new(best.url)
-video.get_mrl()
-vlc_player.set_media(video)
-vlc_player.play()
+textToSearch = re.search('play song (.*)', sys.argv[0])
+if textToSearch:
+    query = urllib.parse.quote(textToSearch.group(1))
+    url = "https://www.youtube.com/results?search_query=" + query
+    response = urllib.request.urlopen(url)
+    html = response.read()
+    soup = BeautifulSoup(html, 'html.parser')
+    song_to_play = ''
+    text_to_speech('searching song on youtube')
+    for vid in soup.findAll(attrs={'class': 'yt-uix-tile-link'}):
+        if 'watch?v=' in vid['href']:
+            song_to_play = vid['href']
+            break
+        else:
+            continue
+    if song_to_play:
+        print(f'song url, https://www.youtube.com{song_to_play}')
+        video_url = pafy.new(f'https://www.youtube.com{song_to_play}')
+        best = video_url.getbest()
+        text_to_speech(f"playing song {textToSearch.group(1)}")
+        vlc_instance = vlc.Instance()
+        vlc_player = vlc_instance.media_player_new()
+        video = vlc_instance.media_new(best.url)
+        video.get_mrl()
+        vlc_player.set_media(video)
+        vlc_player.play()
+    else:
+        print('no song found with matching string')
+        text_to_speech('no song found')
+        pass
+else:
+    pass
